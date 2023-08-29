0ddd875340e3443c67e63af0d219205fc82cbebf
Dave <dlrobjr@gmail.com>
2023-08-29 15:56:47 -0600

    completed game and added functionality using the q,w,a,s keys

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-alert("working");
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
@@ -29,15 +28,63 @@ function nextSequence(){
 
 $(".btn").click(function() {
     var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    animatePress(userChosenColor);
-    playSound(userChosenColor);
+    userInput(userChosenColor);
 })
 
-$(document).keypress(function() {
-    if(!gameStarted){
+function userInput(color){
+    userClickedPattern.push(color);
+    animatePress(color);
+    playSound(color);
+    checkAnswer(userClickedPattern.length - 1);
+}
+
+$(document).keypress(function(letter) {
+    var char = String.fromCharCode(letter.keyCode);
+    if(char === "q" && gameStarted){
+        userInput("green");
+    } else if(char === "w" && gameStarted){
+        userInput("red");
+    } else if(char === "a" && gameStarted){
+        userInput("yellow");
+    } else if(char === "s" && gameStarted){
+        userInput("blue")
+    } else if ((char !== "s" || char !=="q" || char !=="w" || char !=="a") && gameStarted){
+        alert("wrong key");
+    } else{
         nextSequence();
         gameStarted = true;
-    } 
+        userPlaying = true; 
+    }
     
 })
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function() {
+                nextSequence()}, 1000);
+            userClickedPattern = [];
+        }
+    } else {
+        console.log("failed");
+        gameOver();
+    }
+}
+
+function answerWrong(){
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+        $("body").removeClass("game-over");
+    }, 200)
+}
+
+function gameOver(){
+    answerWrong();
+    $("h1").text("Game Over! Press any key to try again.");
+    gameStarted = false;
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];  
+}
