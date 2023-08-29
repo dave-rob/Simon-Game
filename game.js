d4f4cddc824cb9b28304fbf4e15c359e610bd5c5
Dave <dlrobjr@gmail.com>
2023-08-29 07:59:59 -0600

    added more js, flash when clicked and levels go up initialize once button is clicked

--- game.js
+++ game.js
@@ -1,12 +1,43 @@
 alert("working");
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+var gameStarted = false;
+var level = 0;
+
+function playSound(name){
+    var audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(function (){
+        $(`#${currentColor}`).removeClass("pressed")}, 100);
+}
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-    audio.play();
+    level++;
+    $("h1").text(`Level ${level}`);
+    //$(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
+    animatePress(randomChosenColor);
+    playSound(randomChosenColor);
 }
 
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+})
+
+$(document).keypress(function() {
+    if(!gameStarted){
+        nextSequence();
+        gameStarted = true;
+    } 
+    
+})
